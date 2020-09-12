18a3a147209fdcbd08e6b7bcff07d71b3a4e7625
SilviAC04 <silvanaarevalo18@hotmail.com>
2020-09-11 21:05:42 -0500

    programa de nomina

--- ProgramaNomina.py
+++ ProgramaNomina.py
@@ -9,8 +9,20 @@ empleado_3 = {'nombre':'Diana', 'edad': 37, 'telefono':'0992762655','salario': 8
 
 lista_empleados = [empleado_1,empleado_2,empleado_3]
 
+print("----------------------------------------------------------------------------------\n")
+print("\t\tNOMINA DE EMPLEADOS\n")
+print("----------------------------------------------------------------------------------\n")
+
+print("Nombre\t\tEdad\t\tTelefono\t\tSalario\t\tPuesto")
+
 for empleado in lista_empleados:
     nombre_empleado = empleado['nombre']
+    edad_empleado = empleado['edad']
+    telefono_empleado = empleado['telefono']
     salario_empleado = empleado['salario']
+    puesto_empleado = empleado['puesto']
 
-    print(nombre_empleado, salario_empleado)
+    print("{}\t\t{}\t\t{}\t\t{}\t\t{}".format(nombre_empleado,edad_empleado,telefono_empleado,salario_empleado,puesto_empleado))
+
+print("----------------------------------------------------------------------------------\n")    
+   
